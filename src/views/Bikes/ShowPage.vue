4d2514298feb63335c667878101efcd853fbfc04
Michał Stanisławski <michal.stanislawski098@gmail.com>
2023-05-19 01:06:51 +0200

    bike view changes

--- src/views/Bikes/ShowPage.vue
+++ src/views/Bikes/ShowPage.vue
@@ -25,10 +25,13 @@
                 <x-mark-icon class="w-8 h-8 text-white" @click="goToHome" />
             </div>
 
-            <div class="px-3 py-4 rounded-t-[30px] bg-neutral-800 absolute bottom-0 left-0 right-0 w-full flex flex-col gap-6 transition-all duration-500 ease-in-out"
-                @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd"
-                :class="{ 'h-1/4': !isExpanded, 'h-3/4': isExpanded }">
-                <div class="mx-auto w-[40px] h-[5px] rounded-full bg-neutral-700"></div>
+            <div class="z-[20] px-3 py-4 bg-neutral-800 absolute bottom-0 left-0 right-0 w-full flex flex-col gap-6 transition-all duration-500 ease-in-out"
+                @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd" :class="{
+                    'h-1/4 rounded-t-[30px]': !isExpanded,                    
+                    'h-3/4 rounded-t-[30px]': isExpanded && !isFullyExpanded,
+                    'h-full overflow-y-auto rounded-none': isFullyExpanded
+                }">
+                <div class="mx-auto w-[40px] h-[5px] rounded-full bg-neutral-700 shrink-0"></div>
                 <div class="w-full flex items-center justify-between gap-6">
                     <button
                         class="rounded-2xl bg-neutral-700 shadow-lg flex flex-col items-center justify-center gap-[1px] p-2 aspect-square w-full">
@@ -57,14 +60,15 @@
                     <h2 class="fitter-h2 mt-3 mb-6">
                         Tips for you
                     </h2>
-                    <tips-swiper :tips="tips" />
+                    <tips-swiper :tips="tips" />                    
                 </div>
 
                 <div>
                     <h2 class="fitter-h2 mt-3 mb-6">
-                        
+                        Your bike fit
                     </h2>
-                    <tips-swiper :tips="tips" />
+
+                    <apexchart class="w-full" type="line" :options="chartOptions" :series="chartSeries"></apexchart>
                 </div>
 
 
@@ -138,6 +142,7 @@ onMounted(async () => {
 const startY = ref<number>(0)
 const endY = ref<number>(0)
 const isExpanded = ref<boolean>(false)
+const isFullyExpanded = ref<boolean>(false)
 
 function touchStart(e: TouchEvent) {
     console.log('touchStart');
@@ -151,12 +156,77 @@ function touchEnd() {
     console.log('touchEnd');
     const diffY = Number(endY.value) - Number(startY.value);
 
-    if (diffY < -50) {
+    if (!isFullyExpanded.value && !isExpanded.value && diffY < -100) {
         isExpanded.value = true;
+        return;
     }
 
-    if (isExpanded.value && diffY > 50) {
+    if (!isFullyExpanded.value && isExpanded.value && diffY > 100) {
         isExpanded.value = false;
+        return;
+    }
+
+    if (!isFullyExpanded.value && isExpanded.value && diffY < -100) {
+        isFullyExpanded.value = true;
+        return;
+    }
+
+    if (isFullyExpanded.value && isExpanded.value && diffY > 100 && startY.value < 100) {
+        isFullyExpanded.value = false;
+        isExpanded.value = false;
+        return;
     }
 }
+
+
+// chart series of measured angles during bike fit
+const chartSeries = ref<any[]>([{
+    name: 'Bicep angles',    
+    data: [0, 0.5, 0.7, 0.9, 1, 0.9, 0.7, 0.5, 0, -0.5, -0.7, -0.9, -1, -0.9, -0.7, -0.5, 0]
+}, {
+    name: 'Knee angles',
+    // faster and phased sinus
+    data: [0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0, -0.2, -0.4, -0.6, -0.8, -1, -0.8, -0.6, -0.4, -0.2]
+}])
+
+const chartOptions = ref<any>({
+    chart: {
+        type: 'line',
+        height: 350
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        show: true,
+        width: 2        
+    },
+    xaxis: {
+        categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17']
+    },
+    yaxis: {
+        title: {
+            text: 'Angle (°)'
+        }
+    },
+    fill: {
+        opacity: 1
+    },
+    tooltip: {
+        y: {
+            formatter: function (val: number) {
+                return val + "°"
+            }
+        }
+    }
+})
+
+
 </script>
