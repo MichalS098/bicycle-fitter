f4a5de6080aa1361326e4d1828976bcb5f0d7074
pananananas <101266606+pananananas@users.noreply.github.com>
2023-06-21 13:06:10 +0200

    changed graphics

--- src/views/Bikes/MeasureAnglesInstructionsModal.vue
+++ src/views/Bikes/MeasureAnglesInstructionsModal.vue
@@ -3,7 +3,7 @@
         <instruction-modal :show="instructionFirst" prev-button-text="" next-button-text="Next"
             @next-button-action="instructionFirst = false; instructionSecond = true;" 
             title="Measure your angles on bike"
-            image="../../assets/images/instructions/instruction-measure.png" 
+            image="../../assets/images/instructions/luszwost_man_on_a_bike_measuring_knee_angle_minimal_vector_desi_71580842-404e-4e32-8b4c-b794cf71b007.png" 
             content="Now with your phone camera we will 
                     measure your angles in knee, torso etc.
                     This analiysis will give you useful 
@@ -13,7 +13,7 @@
             @next-button-action="instructionSecond = false; instructionThird = true;"
             @prev-button-action="instructionSecond = false; instructionFirst = true;" 
             title="Set up your bike"
-            image="../../assets/images/instructions/instruction-woman.png"
+            image="../../assets/images/instructions/luszwost_man_on_a_bike_measuring_technical_minimal_vector_desig_b1047bb4-3522-4d99-b390-3b608fb138e4 1.png"
             content="You can set up your bike on a holter 
                     or if you dont have one we suggest 
                     lean your bike against a wall or tree 
@@ -23,7 +23,7 @@
             @next-button-action="instructionThird = false; instructionFourth = true;"
             @prev-button-action="instructionThird = false; instructionSecond = true;" 
             title="Start measure on your bike"
-            image="../../assets/images/instructions/instruction-woman.png"
+            image="../../assets/images/instructions/luszwost_woman_on_a_bike_minimal_vector_design_white_background_abf43e57-9015-452e-88b4-7e22179ee87e.png"
             content="When the app sees your entire body it will 
                     start counting down a 15 second timer. 
                     During this time, get safely on your bike 
