e7a1af55a920e38bd317e51e1704b481b56253eb
pananananas <101266606+pananananas@users.noreply.github.com>
2023-06-21 14:35:16 +0200

    changed name to BikeFit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     "appId": "com.bike.fitter.app.project",
-    "appName": "BicycleFitter",
+    "appName": "BikeFit",
     "webDir": "dist",
     bundledWebRuntime: false,
     "plugins": {
